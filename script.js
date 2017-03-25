04ca3e142e779b52de0cd21400e3fd6f3bca21e0
Marc Matvienko <m.matvienko@utexas.edu>
2017-03-25 17:05:14 -0500

    rough overlay

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 $(document).ready(function(){
     var video = $('#video')[0];
-    var jsonFile = "data.json";
-  
-    var scene = {
-	  "x": 0,
-	  "y": 0,
-	  "w": 11000,
-	  "h": 3500
-	};
-		var obj = vid1.eTime;
-	alert(scene.x);
+    var obj;
+    var json = '{"sTime":0,"eTime":60,'+
+    			'"name":"Patagonia","img":"jacket.jpg"}',
+    			 obj = JSON.parse(json);
+	video.addEventListener('seeking', function(){
+		//$('#overlay').css("visibility", "hidden");
+
+	});
 
     video.addEventListener('playing', function(){
-           $('.overlay').fadeOut();
-    })
+        $('#overlay').fadeOut();
+		$('#overlay').css("visibility", "hidden");
+    });
     video.addEventListener('pause', function(){
-           $('.overlay').fadeIn();
-           if(video.currentTime > obj.sTime && 
-           		video.currentTime < obj.eTime){
-           		$('.overlay').update(obj.name);
+		
+	 	if(!video.seeking){         
+           if((video.currentTime > obj.sTime) && (video.currentTime < obj.eTime)){
+           		$('#overlay').css("visibility", "visible");
+				$('#overlay').fadeIn();
+           		document.getElementById('overlay').innerHTML 
+           		= "<div class='outer'>"+
+           		"<div id='left'><div class = 'inner'>"+obj.name + "</div>"+
+           		"<div class = 'inner'><img class='inner' src='"+
+           		obj.img+
+           		"' width='20%'/></div></div>"+
+           		"<div id='right'>Hello</div>"
+           		+"</div>";
            }
-    })
-})
+       }
+    });
+});
