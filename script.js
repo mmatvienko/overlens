b7a255ba62b38db64817c5f3d52a4a8a65d2607b
Ritvik Tandon <rtandon@utexas.edu>
2017-03-25 00:49:26 -0500

    Modified index and script.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 $(function(){
     
     var video = $('#video')[0];
-    
-    video.addEventListener('playing', function(){
-           $('.text').fadeOut();
-    })
-    video.addEventListener('pause', function(){
-           $('.text').fadeIn();
-    })
-    
+	
+	$('#video').click(function(){
+    $('.text').fadeToggle();
+	})
 })
